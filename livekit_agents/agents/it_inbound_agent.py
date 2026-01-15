c91913dc71794c8a93b3dbf098ed774d2f4cc725
Cem Berke Arslan <bscemarslan@gmail.com>
2026-01-15 20:55:57 +0100

    shitty testing #5 I want to fly

--- livekit_agents/agents/it_inbound_agent.py
+++ livekit_agents/agents/it_inbound_agent.py
@@ -606,7 +606,11 @@ async def entrypoint(ctx: JobContext):
         #    base_url="https://openrouter.ai/api/v1",
         #    api_key=os.getenv("OPENROUTER_API_KEY"),
         #)
-        llm=groq.LLM(model="moonshotai/kimi-k2-instruct-0905")
+        llm=openai.LLM(
+            model="kimi-k2-thinking",
+            base_url="https://api.moonshot.ai/v1",
+            api_key=os.getenv("MOONSHOT_API_KEY"),
+        )
         ,
         tts=lk_google.TTS(
             gender="female",
